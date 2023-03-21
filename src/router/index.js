616367ead7c8699c01a5e5a885cbf3bbbf6435e1
VanAnh <anhb1910033@student.ctu.edu.vn>
2023-03-21 21:29:01 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,13 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
