8bec0b200c63a1063069eab09395100e16c892a8
VanAnh <anhb1910033@student.ctu.edu.vn>
2023-04-04 07:50:50 +0700

    Tao trang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,9 +17,13 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
-        },
+    },
+    {
+		path: "/contacts/create",
+		name: "contact.create",
+		component: () => import("@/views/ContactCreate.vue"),
+	},
 ];
-
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/ContactCreate.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <h1 class="text-center"> Tạo liên hệ</h1>
+    <ContactForm @submit:contact="createContact" :contact="contact" class="p-5"></ContactForm>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+import ContactForm from '../components/ContactForm.vue';
+import contactService from '../services/contact.service';
+export default {
+    components: {
+        ContactForm
+    },
+    props: {
+        id: { type: String, required: true },
+    },
+    data() {
+        return {
+            contact: {},
+            message: ""
+        }
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await contactService.create(data)
+                this.message = "Liên hệ được tạo"
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
+
+<style></style>
